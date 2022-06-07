7134ee8d4f45fed434246cfea0a9c2e0d6fee111
Valeria Bonilla <5141@holbertonschool.com>
2022-06-07 07:50:12 -0700

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
+/**
  * main: This C program use puts to print
  * Returns - zero
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	return (0);
 }
