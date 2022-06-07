9cc51ceeb6eecb96ecbbd6a09b825206c7e5e795
Valeria Bonilla <5141@holbertonschool.com>
2022-06-07 07:12:02 -0700

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+ * This program prints exactly Programming is like building a
+ * multilingual puzzle using puts.
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
