4340bccc922feb716d9b0abbeaee6b811bcb45c6
Valeria Bonilla <5141@holbertonschool.com>
2022-06-07 07:58:21 -0700

    Task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - This C program use printf to print
+ * Return: zero
+ */
+int main(void)
+{
+	printf("%s \n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
