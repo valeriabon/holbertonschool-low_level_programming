30785a9f040e2f505e5b6b508b3c374e51878397
Valeria Bonilla <5141@holbertonschool.com>
2022-06-07 19:49:49 -0700

    Task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - makes the alphabet
+ *Return: zero
+ */
+int main(void)
+{
+	char cha;
+
+	for (cha = 'a'; cha <= 'z'; cha++)
+	{
+		putchar(cha);
+	}
+
+	return (0);
+}
