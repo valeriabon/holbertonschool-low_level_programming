f14faccee3e021d5cd55748b66f7cb1b5a80f94a
Valeria Bonilla <5141@holbertonschool.com>
2022-06-07 18:54:15 -0700

    Task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,19 +17,19 @@ int main(void)
 
 	if (m > 5)
 	{
-		printf("Last digit of %u is %u and is greater than 5\n", n, m);
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	}
 	else
 	{
 		if (m == 0)
 		{
-			printf("Last digit of %u is %u and is 0\n", n, m);
+			printf("Last digit of %d is %d and is 0\n", n, m);
 		}
 		else
 		{
 			if (m < 6 && !0)
 			{
-				printf("Last digit of %u is %u and is less than 6 and not 0\n", n, m);
+				printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 			}
 		}
 	}
