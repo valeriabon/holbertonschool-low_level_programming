392352d9ae23032778259fc9a39f9c23bda56504
Valeria Bonilla <5141@holbertonschool.com>
2022-06-07 18:49:00 -0700

    Task 1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - This C program tells if the remainder of a random number is positive, zero or negative
+ * Return: zero
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+		/* your code goes there */ 
+	m = n % 10;
+
+	if (m > 5)
+	{
+		printf("Last digit of %u is %u and is greater than 5\n", n, m);
+	}
+	else
+	{
+		if (m == 0)
+		{
+			printf("Last digit of %u is %u and is 0\n", n, m);
+		}
+		else
+		{
+			if (m < 6 && !0)
+			{
+				printf("Last digit of %u is %u and is less than 6 and not 0\n", n, m);
+			}
+		}
+	}
+
+	return (0);
+}
