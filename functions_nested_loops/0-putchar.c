0e547f810ed487121ba43d091f988e98b55d8980
Valeria Bonilla <5141@holbertonschool.com>
2022-06-09 10:10:35 -0700

    Task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,20 @@
-include<stdio.h>
+#include <stdio.h>
+#include "main.h"
 /**
  * main - program that prints _putchar, followed by a new line
+ * program - loop over the array
  * Return: zero
  */
 int main(void)
 {
-	
+	int i = 0;
+        char str[7] = {'_','p','u','t','c','a','r'};
+
+        while (i <= 7)
+        {
+                _putchar(str[i]);
+                i++;
+        }
+        _putchar('\n');
+	return (0);
 }
